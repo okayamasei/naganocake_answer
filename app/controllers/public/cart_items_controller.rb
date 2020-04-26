ac31ae56e9ee24a9b768d093d26fc86efc835bc0
Ito <atsushi.itoh66@gmail.com>
2020-04-27 02:59:56 +0900

    add: #4 カート機能[エンドユーザ#商品一覧, エンドユーザ#商品詳細, エンドユーザ#カート内商品一覧]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,19 +1,58 @@
 class Public::CartItemsController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_end_user!
 
   def index
+    @cart_items = CartItem.where(end_user_id: current_end_user.id)
+    @total = 0
+    @cart_items.map { |r| @total += r.subtotal }
   end
 
   def create
+    previous_cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id], end_user_id: current_end_user.id)
+    if previous_cart_item.nil?
+      logger.info("新規")
+      @cart_item = current_end_user.cart_items.new(cart_item_params)
+    else
+      logger.info("追加")
+      @cart_item = previous_cart_item
+      @cart_item.amount += params[:cart_item][:amount].to_i
+    end
 
+    if @cart_item.save
+      @cart_items = CartItem.where(end_user_id: current_end_user.id)
+      redirect_to public_cart_items_index_path
+    else
+      redirect_to public_item_path(params[:cart_item][:item_id])
+    end
+  end
 
   def update
+    cart_item = CartItem.find_by(id: params[:id], end_user_id: current_end_user.id)
+    if cart_item.nil?
+      redirect_to public_cart_items_index_path
+    end
+    cart_item.update(cart_item_params)
+    redirect_to public_cart_items_index_path
   end
 
   def destroy
+    cart_item = CartItem.find_by(id: params[:id], end_user_id: current_end_user.id)
+    if cart_item.destroy
+      redirect_to public_cart_items_index_path
+    else
+      redirect_to public_cart_items_index_path
+    end
   end
 
   def destroy_all
+    cart_items = CartItem.where(end_user_id: current_end_user.id)
+    cart_items.destroy_all
+    redirect_to public_cart_items_index_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,12 @@
 class Public::ItemsController < ApplicationController
+  # before_action :authenticate_end_user!, only: [:index, :show]
   def top
   end
 
   def index
     if params[:genre_id].present?
       @items = Item.where(genre_id: params[:genre_id])
+      @genre = Genre.find(params[:genre_id])
     else
       @items = Item.all
     end
