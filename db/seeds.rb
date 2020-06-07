488dd0eaaa2733217b5e8aea501d0585dce389a0
Ito <atsushi.itoh66@gmail.com>
2020-06-08 02:36:27 +0900

    add: db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(email: 'admin@admin', password: '12345678').save
+Admin.create(email: 'admin@admin', password: '12345678').save if Admin.find_by(email: 'admin@admin').nil?
 
-Genre.create(name: 'ケーキ')
+Genre.create(name: 'ケーキ') if Genre.find_by(name: 'ケーキ').nil?
+Genre.create(name: 'その他') if Genre.find_by(name: 'その他').nil?
 
