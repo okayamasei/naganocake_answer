9b9cbb93a8d6592f7e002c32d2bab048fcd6b711
Ito <atsushi.itoh66@gmail.com>
2020-06-07 16:22:03 +0900

    fix: validation, design

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,10 @@ class Item < ApplicationRecord
   attachment :image
 
   enum sales_status: { sale: 1, sold_out: 2 }
+  validates :name, presence: true
   validates :genre_id, presence: true
+  validates :price, presence: true
+  validates :sales_status, presence: true
 
   def price_with_tax
     (self.price * 1.10).to_i
