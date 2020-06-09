a0f6a798d1684c316f358778b0863b43e66592e2
Ito <atsushi.itoh66@gmail.com>
2020-06-09 23:21:41 +0900

    add: Timezone

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module NaganoCake
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
 
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       if instance.kind_of?(ActionView::Helpers::Tags::Label)
